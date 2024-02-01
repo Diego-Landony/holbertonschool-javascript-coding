258d6295eeb877ebde434ed5d6d4e210bfce1da6
Diego Landony Lima Toledo <132611526+Diego-Landony@users.noreply.github.com>
2024-02-01 16:47:28 -0600

    a

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,29 +1,66 @@
 const express = require('express');
-const students = require('./3-read_file_async');
+const fs = require('fs');
 const app = express();
-const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
-    res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+app.get('/students', (req, res) => {
+  const database = process.argv[2];
+  const fields = ['firstname', 'lastname', 'age', 'field'];
+  const validFields = ['firstname', 'lastname', 'age', 'field'];
+  const validFieldsSet = new Set(validFields);
+  const validFieldsString = validFields.join(', ');
+  const students = [];
+
+  if (!database) {
+    res.status(500).send('Database file not specified');
+    return;
+  }
+
+  fs.readFile(database, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Cannot load the database');
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+
+    for (const line of lines) {
+      const values = line.split(',');
+      const student = {};
+
+      for (let i = 0; i < values.length; i++) {
+        const field = fields[i];
+        const value = values[i].trim();
+
+        if (validFieldsSet.has(field)) {
+          student[field] = value;
+        }
+      }
+
+      if (Object.keys(student).length === validFields.length) {
+        students.push(student);
+      }
+    }
+
+    const nbStudents = students.length;
+    const csStudents = students.filter(s => s.field === 'CS');
+    const sweStudents = students.filter(s => s.field === 'SWE');
+    const nbCsStudents = csStudents.length;
+    const nbSweStudents = sweStudents.length;
+    const csStudentsList = csStudents.map(s => s.firstname).join(', ');
+    const sweStudentsList = sweStudents.map(s => s.firstname).join(', ');
+    const response = `This is the list of our students\nNumber of students: ${nbStudents}\nNumber of students in CS: ${nbCsStudents}. List: ${csStudentsList}\nNumber of students in SWE: ${nbSweStudents}. List: ${sweStudentsList}`;
+
+    res.send(response);
+  });
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
